07b182654a63c404a976c26bb6d794c7e73c8b63
Dr.Ho1y <dr.holyblack@gmail.com>
2025-01-20 23:10:38 +0700

    end edit

--- src/api.js
+++ src/api.js
@@ -103,7 +103,6 @@ async function getBtcPrice() {
             headers: {
                 Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                 "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-                'Cookie': '_ga=GA1.1.2143701187.1737267663; ASP.NET_SessionId=t2ixvifbjyfuhvsyo5cdbqh4; _ga_RXVWB6WQ9C=GS1.1.1737385499.4.1.1737385503.0.0.0',
                 'Priority': 'u=0,i'
             }
         })
@@ -127,7 +126,6 @@ async function getUSDTRub() {
             headers: {
                 Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-                'Cookie': '_ga=GA1.1.2143701187.1737267663; ASP.NET_SessionId=t2ixvifbjyfuhvsyo5cdbqh4; _ga_RXVWB6WQ9C=GS1.1.1737385499.4.1.1737385503.0.0.0',
                 'Priority': 'u=0,i'
             }
         })
@@ -163,7 +161,6 @@ async function getThReward(pow, eff, cost, usdcurs, num, quan) {
             headers: {
                 Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-                'Cookie': '_ga=GA1.1.2143701187.1737267663; ASP.NET_SessionId=t2ixvifbjyfuhvsyo5cdbqh4; _ga_RXVWB6WQ9C=GS1.1.1737385499.4.1.1737385503.0.0.0',
                 'Priority': 'u=0,i'
             },
             params: {
